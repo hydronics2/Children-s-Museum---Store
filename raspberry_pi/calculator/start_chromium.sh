eb65c5d35f6cba1e23151b9bd6d15ff54ae0eccb
OMSI\thudson <hydronics@gmail.com>
2017-08-03 14:26:06 -0700

    Signed-off-by: OMSI\thudson <hydronics@gmail.com>

--- raspberry_pi/calculator/start_chromium.sh
+++ raspberry_pi/calculator/start_chromium.sh
@@ -1,25 +1,28 @@
+
 #notes:
-#startup description: https://www.raspberrypi.org/forums/viewtopic.php?t=163316&p=1055788
 
 #apt-get install unclutter
 #apt-get install xdotool
 
+#put in home directory... seems to work best there!
+#sudo mv start_chromium.sh /home/pi/
+#sudo apt-get install dos2unix (this is to change wierd windows characters to unix compatiable as needed)
+#dos2unix start_chromium.sh
 
-
-#sudo nano start_chromium.sh
-#add chromium-browser start command below
-#sudo chmod 755 start_chromium.sh
-#sudo chmod +x start_chromium.sh
+#
+#chmod 755 start_chromium.sh
+#
 
 # Run browser after boot to desktop
 /bin/sleep 3
-sudo -u pi chromium-browser --kiosk --incognito /home/pi/Portlands-Childrens-Museum---Grocery/raspberry\ pi/calculator/index.html &
+sudo -u pi chromium-browser --kiosk --incognito /home/pi/PCM/raspberry_pi/calculator/index.html &
 # End of script
 
 
 # Now your scripts are for sure executable.  ;) Next the autostart file needs to be modified to make use of our scripts and the following commands added to the end of the file and in the specific order you see them here:
-# nano /home/pi/.config/lxsession/LXDE-pi/autostart
-# @/home/pi/Portlands-Childrens-Museum---Grocery/raspberry\ pi/calculator/start_chromium.sh
+# nano /home/pi/.config/lxsession/LXDE-pi/autostart ....................in lieu of /etc/xdg/lxsessions/LXDE-pi/autostart
+#no @ sign needed
+# /home/pi/start_chromium.sh
 
 #Now don't close the file just yet, you need to comment out one line with a #, so edit to look like this:
 # @xscreensaver -no-splash
